4a21f31a3e5102cab648ce61fa4f55f60a550d6f
CodeNinja1395 <38698791+CodeNinja1395@users.noreply.github.com>
2018-06-19 18:39:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.use(cors());
 
 
 require('./src')(app);
-app.listen(3000, () => {
-  console.log('Server started on port 3000...');
+app.listen(80, () => {
+  console.log('Server started on port 80...');
 });
